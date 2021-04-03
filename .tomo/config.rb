a19de8fad6ed51a2da9229ef23da0ea09d122de3
Francois Jouvray <francois0690@gmail.com>
2021-04-03 18:16:53 +0200

    nokogiri

---
+++ .tomo/config.rb
@@ -0,0 +1,77 @@
+# rubocop:disable Style/FormatStringToken
+plugin "git"
+plugin "env"
+plugin "bundler"
+plugin "rails"
+plugin "nodenv"
+plugin "puma"
+plugin "rbenv"
+plugin "./plugins/cats-deploy.rb"
+
+host "deployer2@192.168.1.19"
+
+set application: "cats-deploy"
+set deploy_to: "/var/www/%{application}"
+set nodenv_node_version: "10.19.0"
+set nodenv_yarn_version: "1.22.5"
+set git_url: "https://github.com/francois0690/cats-deploy.git"
+set git_branch: "master"
+set puma_port: "3000"
+set git_exclusions: %w[
+  .tomo/
+  spec/
+  test/
+]
+set env_vars: {
+  RACK_ENV: "production",
+  RAILS_ENV: "production",
+  RAILS_LOG_TO_STDOUT: "1",
+  RAILS_SERVE_STATIC_FILES: "1",
+  CLOUDINARY: :prompt,
+  DATABASE_URL: :prompt,
+  SECRET_KEY_BASE: :prompt
+}
+set linked_dirs: %w[
+  log
+  node_modules
+  public/assets
+  public/packs
+  tmp/cache
+  tmp/pids
+  tmp/sockets
+]
+
+setup do
+  run "env:setup"
+  run "core:setup_directories"
+  run "git:clone"
+  run "git:create_release"
+  run "core:symlink_shared"
+  run "nodenv:install"
+  run "rbenv:install"
+  run "bundler:upgrade_bundler"
+  run "bundler:config"
+  run "bundler:install"
+  run "rails:db_create"
+  run "rails:db_schema_load"
+  run "rails:db_seed"
+  run "puma:setup_systemd"
+end
+
+deploy do
+  run "env:update"
+  run "git:create_release"
+  run "core:symlink_shared"
+  run "core:write_release_json"
+  run "bundler:install"
+  run "rails:db_migrate"
+  run "rails:db_seed"
+  run "rails:assets_precompile"
+  run "core:symlink_current"
+  run "puma:restart"
+  run "puma:check_active"
+  run "core:clean_releases"
+  run "bundler:clean"
+  run "core:log_revision"
+end
+# rubocop:enable Style/FormatStringToken
